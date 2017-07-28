6878a0117edd110ca1050197b210b65e3889b81c
Mauderica <maude.chalin@gmail.com>
2017-07-28 17:06:34 -0400

    Injecting HTML with jQuery - first try
    We make use of our helper.js file (which is importantly listed before resumeBuilder.js in index.html). In resumeBuilder.js, we created new variables that reference variables in the helper.js file (which contain lines of HTML) and edit them using .replace(). These new variables (which are lines of HTML with content which we modified) are then injected into our HTML using jQuery's .prepend().

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,18 +3,21 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 
 // $("#main").append("Maude Chalin");
-
 // var firstName = "Maude";
 // var age = 27;
-
 // console.log(firstName);
+// var awesomeThoughts = "I am Maude and I am AWESOME!";
+// console.log(awesomeThoughts);
+// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+// console.log(funThoughts);
+// $("#main").append("Hello, " + funThoughts);
 
-var awesomeThoughts = "I am Maude and I am AWESOME!";
+var name = "Maude Chalin";
+var role = "Front-End Web Developer";
 
-console.log(awesomeThoughts);
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
 
-var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-
-console.log(funThoughts);
-
-$("#main").append("Hello, " + funThoughts);
+// $("#header").append(formattedName);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName); // Using .prepend( ) in this order will ensure the name appears before the role in the HTML.
