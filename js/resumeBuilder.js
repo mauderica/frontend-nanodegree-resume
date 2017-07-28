f65de5b8f2eb5f9a862cfc2116cf982248bd5b1e
Mauderica <maude.chalin@gmail.com>
2017-07-28 11:10:57 -0400

    Practice with JS, jQuery, console.log( )

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,20 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+// $("#main").append("Maude Chalin");
+
+// var firstName = "Maude";
+// var age = 27;
+
+// console.log(firstName);
+
+var awesomeThoughts = "I am Maude and I am AWESOME!";
+
+console.log(awesomeThoughts);
+
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+console.log(funThoughts);
+
+$("#main").append("Hello, " + funThoughts);
