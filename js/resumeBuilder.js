a1c4eeaa36ec7b62c04911f232e9bc0b681d22f8
Mauderica <maude.chalin@gmail.com>
2017-08-01 12:07:32 -0400

    File clean-up
    Deleted practice code from quizzes in order to have a "clean" file in preparation for writing the code that will build my resume page. Finished creating the bio, education, work and projects objects.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,31 +1,7 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
-
-// $("#main").append("Maude Chalin");
-// var firstName = "Maude";
-// var age = 27;
-// console.log(firstName);
-// var awesomeThoughts = "I am Maude and I am AWESOME!";
-// console.log(awesomeThoughts);
-// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-// console.log(funThoughts);
-// $("#main").append("Hello, " + funThoughts);
-
-var name = "Maude Chalin";
-var role = "Front-End Web Developer";
-
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-
-// $("#header").append(formattedName);
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName); // Using .prepend( ) in this order will ensure the name appears before the role in the HTML.
-
 // THIS IS MY BIO OBJECT
 var bio = {
-    "name" : name,
-    "role" : role,
+    "name" : "Maude Chalin",
+    "role" : "Front-End Web Developer",
     "contacts" : {
         "mobile" : "785-289-8904",
         "email" : "maude.chalin@gmail.com",
@@ -42,59 +18,99 @@ var bio = {
     "biopic" : "images/me.jpeg"
 };
 
-// Not sure if need this... var formattedContactGeneric = HTMLcontactGeneric.replace("%data%", bio.contacts);
-var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
-var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-var formattedSkills = HTMLskills.replace("%data%", bio.skills);
-var formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic);
 
-$("#topContacts").append(
-    formattedMobile,
-    formattedEmail,
-    formattedGithub,
-    formattedLocation,
-    formattedWelcomeMsg,
-    formattedSkills,
-    formattedBioPic
-);
+// var formattedName = HTMLheaderName.replace("%data%", bio.name);
+// var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+// // var formattedContactGeneric = HTMLcontactGeneric.replace("%data%", bio.contacts);  // Not sure if need and why
+// var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+// var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+// var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+// var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+// var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+// var formattedSkills = HTMLskills.replace("%data%", bio.skills);
+// var formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic);
 
-// THIS IS MY WORK OBJECT --> DOT NOTATION PRACTICE
-var work = {};
-work.jobs = [
-    {
-        "employer" : "Star Fleet",
-        "title" : "Ensign",
-        "location" : "Alpha Quadrant",
-        "dates" : "In progress",
-        "description" : "Best job ever!"
-    },
-    {
-        "employer" : "NASA",
-        "title" : "Astronaut in Training",
-        "location" : "San Francisco, Earth",
-        "dates" : "2022-2024",
-        "description" : "Assistant to pilot a starship."
-    }
-];
+// $("#header").prepend(formattedRole);
+// $("#header").prepend(formattedName); // Using .prepend( ) in this order will ensure the name appears before the role in the HTML.
+
+// $("#topContacts").append(
+//     formattedMobile,
+//     formattedEmail,
+//     formattedGithub,
+//     formattedLocation,
+//     formattedWelcomeMsg,
+//     formattedSkills,
+//     formattedBioPic
+// );
 
 
-// THIS IS MY EDUCATION OBJECT --> BRACKET NOTATION PRACTICE
-var education = {};
-education["schools"] = [
-    {
-        "name" : "Star Fleet Academy",
-        "location" : "San Francisco, CA",
-        "degree" : "Computer Engineering",
-        "majors" : [
-            "Astrophysics", "Exolinguistics"
-        ],
-        "dates" : "2018-2022",
-        "url" : "www.starfleetacademy.org"
-    }
-];
+// THIS IS MY EDUCATION OBJECT
+var education = {
+    "schools" : [
+        {
+            "name" : "Star Fleet Academy",
+            "location" : "San Francisco, CA",
+            "degree" : "BSc. Computer Engineering",
+            "majors" : [
+                "Astrophysics", "Exolinguistics"
+            ],
+            "dates" : "2018-2022",
+            "url" : "www.starfleetacademy.org"
+        },
+        {
+            "name" : "Hogwarts School of Witchcraft and Wizardry",
+            "location" : "Beyond platform 9 3/4",
+            "degree" : "MS Astrology",
+            "majors" : [
+                "Potions", "Spells"
+            ],
+            "dates" : "2022-2024",
+            "url" : "www.Hogwarts.org"
+        }
+    ],
+    "onlineCourses" : [
+        {
+            "title" : "Front-End Wed Developer Nanodegree",
+            "school" : "Udacity",
+            "dates" : "2017",
+            "url" : "www.udacity.com"
+        }
+    ]
+};
 
-$("#main").append(work.jobs[0].title);
-$("#main").append(education["schools"][0]["name"]);
+
+// THIS IS MY WORK OBJECT
+var work = {
+    "jobs" : [
+        {
+            "employer" : "Star Fleet",
+            "title" : "Ensign",
+            "location" : "Alpha Quadrant",
+            "dates" : "In progress",
+            "description" : "Best job ever!"
+        },
+        {
+            "employer" : "NASA",
+            "title" : "Astronaut in Training",
+            "location" : "San Francisco, Earth",
+            "dates" : "2022-2024",
+            "description" : "Assistant to pilot a starship."
+        }
+    ]
+};
+
+
+// THIS IS MY PROJECTS OBJECT
+var projects = {
+    "projects" : [
+        {
+            "title" : "Portfolio Site",
+            "dates" : "2017-2018",
+            "description" : "I created a first draft of my first portfolio site using HTML and CSS and responsive design techniques.",
+            "images" : [
+                "www.projectimage1.com",
+                "www.projectimage2.com"
+            ]
+        }
+    ]
+};
